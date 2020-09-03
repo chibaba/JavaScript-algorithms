c26f851c4875fc211bc987a1e138ebbaa90f3c9f
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-04 00:51:51 +0100

    wip

--- task_1.js
+++ task_1.js
@@ -4,16 +4,16 @@
 //  should hold the sum of the even integers and
 //  the second index should hold the sum of the odd integers.
 
-function isAdd(n) {
-    let arr = [];
-    if (n % 2 === 0) {
-        for (var i = 0; i < n.length; i++) return (arr += i);
-    }
-    if (n % 2 !== 0) {
-        for (var j = 0; j < n.length; j++) return (arr += j);
-    }
-    return (arr = [i, j]);
-}
+// function isAdd(n) {
+//     let arr = [];
+//     if (n % 2 === 0) {
+//         for (var i = 0; i < n.length; i++) return (arr += i);
+//     }
+//     if (n % 2 !== 0) {
+//         for (var j = 0; j < n.length; j++) return (arr += j);
+//     }
+//     return (arr = [i, j]);
+// }
 
 function isSum(n) {
     let arr = [];
